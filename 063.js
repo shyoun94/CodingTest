de42f5b1bf5067231920446b3da68a9d48abbc85
prefer_youn <shyoun94@gmail.com>
2023-07-05 21:35:39 +0900

    JS 코딩테스트 연습문제

--- 063.js
+++ 063.js
@@ -5,3 +5,12 @@
 
 // 교수님이 줄임말을 배우기 위해 아래와 같이 **어떤 입력이 주어지면 앞 글자만 줄여 출력**하도록 해주세요.
 // 입력은 한글 혹은 영어로 입력되며, 띄어쓰기를 기준으로 하여 짧은 형태로 출력합니다.
+
+const input = prompt('문자열을 입력하세요').split(' ');
+let result = '';
+
+for (let i of input){
+  result += i.slice(0, 1);
+}
+
+console.log(result);
