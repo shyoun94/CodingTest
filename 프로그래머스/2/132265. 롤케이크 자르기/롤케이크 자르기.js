5415640cd9fe7e8aa9556defcb5367b6613b5283
prefer_youn <38209666+shyoun94@users.noreply.github.com>
2024-01-04 21:39:26 +0900

    [level 2] Title: 롤케이크 자르기, Time: 15.66 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,22 @@
+function solution(topping) {
+    let answer = 0;
+    let left = new Array(10001).fill(0);
+    let right = new Array(10001).fill(0);
+    let leftCount = 0;
+    let rightCount = 0;
+
+    for (let i = 0; i < topping.length; i++) {
+        right[topping[i]]++;
+        if (right[topping[i]] === 1) rightCount++;
+    }
+
+    for (let i = 0; i < topping.length - 1; i++) {
+        left[topping[i]]++;
+        if (left[topping[i]] === 1) leftCount++;
+        if (right[topping[i]]-- === 1) rightCount--;
+
+        if (leftCount === rightCount) answer++;
+    }
+
+    return answer;
+}
