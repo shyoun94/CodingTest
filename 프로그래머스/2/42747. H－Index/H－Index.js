232ad84deff7417d80483462eb1f504144b9bb0d
prefer_youn <38209666+shyoun94@users.noreply.github.com>
2024-02-13 23:04:23 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,17 @@
+function solution(citations) {
+    var answer = 0;
+    
+    citations.sort((a,b)=> b-a);
+
+    let arr = [];
+    for(let i=0; i<citations.length; i++){
+        arr.push(citations[i]);
+
+        if(citations[i]<arr.length){
+            break;
+        }
+        answer++;
+    }
+    
+    return answer;
+}
